7ea349fae27d019e62ebba958b324bb4177ce463
Salmani <salmanisahel79@gmail.com>
2021-05-26 19:49:38 +0430

    finish login & signup activity ui-second part

--- app/src/main/java/com/example/myapphome/LoginActivity.java
+++ app/src/main/java/com/example/myapphome/LoginActivity.java
@@ -2,13 +2,33 @@ package com.example.myapphome;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity{
 
+
+    EditText emailbox,passwordbox;
+    Button loginbtn,signbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        emailbox=findViewById(R.id.emailbox);
+        passwordbox=findViewById(R.id.passwordbox);
+
+        loginbtn=findViewById(R.id.loginbtn);
+        signbtn=findViewById(R.id.creatbtn);
+
+        signbtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this,SignUpActivity.class));
+            }
+        });
     }
 }
